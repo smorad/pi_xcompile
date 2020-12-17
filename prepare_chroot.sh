3b114a108a125577b519a0fd9eba65451cf7b5c3
Steven Morad <stevenmorad@gmail.com>
2020-12-17 03:09:57 -0700

    Add ray rllib deps

--- prepare_chroot.sh
+++ prepare_chroot.sh
@@ -11,11 +11,17 @@ cp /etc/passwd etc/passwd
 # Networking
 cp /etc/resolv.conf etc/resolv.conf
 # Certs for pip3
-cp /etc/ssl/certs/ca-certificates.crt etc/ssl/certs/ca-certificates
+cp /etc/ssl/certs/ca-certificates.crt etc/ssl/certs/ca-certificates.crt
+
 # Pytorch specific stuff
 git clone https://github.com/pytorch/pytorch
 pushd pytorch
 git checkout origin/1.7
 git submodule update --init --recursive
 popd
+
+# Ray rllib specific stuff
+git clone https://github.com/ray-project/ray.git
+ray/ci/travis/install-bazel.sh
+
 popd
